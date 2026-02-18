3eff3c8650fc07aa07cc3b7a8975d9620e34b698
Pawan Dangi <dangi.pawan2001@gmail.com>
2026-02-18 18:06:07 +0530

    scroll to top added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import {BrowserRouter }from 'react-router-dom'
 import Header from './components/layout/Header/Header'
 import AppRoutes from './constants/AppRoutes' 
 import Footer from './components/layout/Footer/Footer'
+import ScrollToTop from './constants/ScrollToTop'
 function App() {
 
 
   return (
     <>
     <BrowserRouter>
+    <ScrollToTop/>
     <Header/>
     <AppRoutes/>
     <Footer/>
